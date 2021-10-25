75fa22e2afaa13ab5814b2cf2899eded32fda642
d3n <d3n@d3n.it>
2021-10-25 23:14:54 +0200

    feat: Remove unused select2

--- resources/views/issues/create.blade.php
+++ resources/views/issues/create.blade.php
@@ -3,8 +3,7 @@
     <link rel="stylesheet" href="//cdn.jsdelivr.net/editor/0.1.0/editor.css">
     <script src="//cdn.jsdelivr.net/editor/0.1.0/editor.js"></script>
     <script src="//cdn.jsdelivr.net/editor/0.1.0/marked.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
     <script>
         $(document).ready(_ => {
 
