20ad429406b438fb67f5a4f924cf3ddc93aaaaf9
d3n <d3n@d3n.it>
2021-11-01 11:48:06 +0100

    feat: Flash message when the proposal was rejected.

--- app/Http/Livewire/PaidServiceForm.php
+++ app/Http/Livewire/PaidServiceForm.php
@@ -13,22 +13,23 @@ class PaidServiceForm extends ModalComponent
 
     public function accept()
     {
-        $url=$this->ps->accpetProposal();
+        $url = $this->ps->accpetProposal();
         return Redirect::to($url);
     }
 
     public function reject()
     {
-        //not yet implemented
+        $this->ps->rejectProposal();
+        session()->flash('message','Proposal declined.');
     }
 
     public function mount()
     {
-        $this->ps = PaidService::where('issue', $this->issue)->first();
+        $this->ps = PaidService::where('issue', $this->issue)->where('status', 0)->first();
     }
 
     public function render()
     {
-       return view('livewire.paid-service-form');
+        return view('livewire.paid-service-form');
     }
 }
